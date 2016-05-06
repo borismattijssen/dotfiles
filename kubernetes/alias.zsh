313f5b4577b47a000ef1186b8f4de87bd718b272
Boris Mattijssen <borismattijssen@gmail.com>
2016-05-06 11:22:43 +0200

    Alias to start kubie

--- kubernetes/alias.zsh
+++ kubernetes/alias.zsh
@@ -1,3 +1,4 @@
 if [[ -a ~/dev/go/src/k8s.io/kubernetes/cluster/kubectl.sh ]]; then
   alias lkubectl=~/dev/go/src/k8s.io/kubernetes/cluster/kubectl.sh 
+  alias kubestart='sudo env "PATH=$PATH" ~/dev/go/src/k8s.io/kubernetes/hack/local-up-cluster.sh'
 fi
