8cfe7ee37931de435257e991f31cb7d332286214
borismattijssen <borismattijssen@gmail.com>
2020-03-17 16:05:11 +0100

    Groups aliases

---
+++ system/aliases.zsh
@@ -0,0 +1,17 @@
+# MISC
+alias ea='vim ~/.dotfiles/system/aliases.zsh'
+alias reload!='. ~/.zshrc'
+alias dockertime='docker run -it --rm --privileged --pid=host debian nsenter -t 1 -m -u -n -i date -u $(date -u +%m%d%H%M%Y)'
+alias se='source .env/bin/activate'
+
+# GIT
+alias gl='git pull'
+alias gp='git push'
+alias gd='git diff'
+alias gc='git commit -m'
+alias gca='git commit -a -m'
+alias gco='git checkout'
+alias gcb='git checkout -b'
+alias gs='git status'
+alias gac='git add -A && git commit -m'
+
--- zsh/aliases.zsh
+++
@@ -1,3 +0,0 @@
-alias reload!='. ~/.zshrc'
-alias dockertime='docker run -it --rm --privileged --pid=host debian nsenter -t 1 -m -u -n -i date -u $(date -u +%m%d%H%M%Y)'
-alias se='source .env/bin/activate'
