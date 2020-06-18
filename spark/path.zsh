809b2dd80243c5a1c11e19e7e1f84cf940e7fec9
borismattijssen <borismattijssen@MBP-borismattijssen.local>
2020-06-18 17:18:18 +0200

    Only add spark to the python path if spark is installed on the machine

--- spark/path.zsh
+++ spark/path.zsh
@@ -1,2 +1,4 @@
-export SPARK_HOME=/usr/local/Cellar/apache-spark/2.4.0/libexec
-export PYTHONPATH=/usr/local/Cellar/apache-spark/2.4.0/libexec/python/:$PYTHONP$
+if [[ -d '/usr/local/Cellar/apache-spark' ]]; then
+	export SPARK_HOME="/usr/local/Cellar/apache-spark/2.4.0/libexec"
+	export PYTHONPATH="/usr/local/Cellar/apache-spark/2.4.0/libexec/python/:$PYTHONPATH"
+fi
